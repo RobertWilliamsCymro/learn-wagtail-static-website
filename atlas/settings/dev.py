ad1b2dc8b2ce81461cebf6f7975a13f45ba2e1c6
Clare Dunne <claredunne@jmrwjc256644-ce.home>
2024-05-20 07:02:53 +0100

    Initial setup

---
+++ atlas/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-po(s*fq&1a6r+&09*6@@0d8@q1gd3!u+*k=)hbn)xa^$h9&^70"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
